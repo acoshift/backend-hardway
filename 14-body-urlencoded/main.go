635132ff63d0d2493b522c16ae66a2b3ccdd1b38
acoshift <acoshift@gmail.com>
2017-10-20 19:59:18 +0700

    add 14

--- 14-body-urlencoded/main.go
+++ 14-body-urlencoded/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
@@ -15,5 +16,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	buf := bytes.Buffer{}
 	io.Copy(&buf, r.Body)
 	log.Println(buf.String())
+	body, err := url.ParseQuery(buf.String())
+	if err != nil {
+		log.Println("parse error:", err)
+		return
+	}
+	log.Println(body)
 	w.Write([]byte("ok\n"))
 }
